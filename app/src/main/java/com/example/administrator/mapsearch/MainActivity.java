8583d253de75e306510e764f8a72e4989f58cf50
yahfard <yahfard-bin@hotmail.com>
2016-02-15 13:24:53 +0700

    เพิ่มเสียง และ วาด Polygon ได้หมดแล้ว

--- app/src/main/java/com/example/administrator/mapsearch/MainActivity.java
+++ app/src/main/java/com/example/administrator/mapsearch/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.administrator.mapsearch;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.graphics.Color;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap googleMap;
     ImageView imageView;
+
+    ArrayList<LatLng> positionList = new ArrayList<>();
+    Stack<Marker> markers = new Stack<>();
+    private Polygon polygon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +42,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         addMarkerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                googleMap.addMarker(new MarkerOptions().position(
-                        googleMap.getCameraPosition().target));
+                addMarker();
+                createPolygon();
             }
         });
+
         imageView = (ImageView) findViewById(R.id.image);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                googleMap.addMarker(new MarkerOptions().position(
-                        googleMap.getCameraPosition().target));
-
+                addMarker();
+                createPolygon();
             }
         });
+
+        final Button clearMarker = (Button) findViewById(R.id.clearMarker);
+        clearMarker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              if (!markers.isEmpty()){
+
+                  markers.peek().remove();
+                  markers.pop();
+                  positionList.remove(positionList.size() - 1);
+              }
+                if (!positionList.isEmpty())
+                createPolygon();
+            }
+        });
+
+
+    }
+
+    private void addMarker() {
+        Marker marker = googleMap.addMarker(new MarkerOptions().position(
+                googleMap.getCameraPosition().target));
+        markers.push(marker);
+        positionList.add(googleMap.getCameraPosition().target);
+
+        MediaPlayer mpEffect
+                = MediaPlayer.create(MainActivity.this, R.raw.sound);
+        mpEffect.start();
+    }
+
+
+    public void createPolygon() {
+        PolygonOptions rectGon = new PolygonOptions();
+        rectGon.addAll(positionList)
+                .strokeColor(Color.RED)
+                .fillColor(Color.YELLOW)
+                .strokeWidth(3);
+        if (polygon != null)
+            polygon.remove();
+        polygon = googleMap.addPolygon(rectGon);
     }
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         this.googleMap = googleMap;
+
         googleMap.getUiSettings().setZoomControlsEnabled(true);
     }
-
 }
